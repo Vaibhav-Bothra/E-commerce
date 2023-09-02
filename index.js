c68a4170ad8d38f7faa3ec110af0bb6376d6c12c
Vaibhav <vaibhavbothra2004@gmail.com>
2023-09-03 03:20:38 +0530

    Feat: Frontend of home page is developed.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+//git init for making git commits
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','views');
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(express.urlencoded());
+app.use(express.static('./assets'));
+
+app.use('/',require('./routes/index'));
+
+app.listen(port,function(err){
+    if(err) console.log(`Error: ${err}`);
+    console.log("Successfully set up the server.");
+});
